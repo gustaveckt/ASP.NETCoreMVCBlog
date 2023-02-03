c2b1df1825c346fdb03bab4c05a9c191b18229d4
Gustave TSOPMO <gustavetsopmo.gt@gmail.com>
2023-02-03 21:32:28 +0100

    Adding Admin dashboard

---
+++ Blog/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Static;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [Authorize(Roles = AppRoles.Admin)]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
